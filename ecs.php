26bc83e244e7aa2cd6c207ce3f068d56e1a803a5
Julián Gutiérrez <juliangut@gmail.com>
2023-12-09 18:44:53 +0100

    update file notices and add ecs cache

--- ecs.php
+++ ecs.php
@@ -1,12 +1,10 @@
 <?php
 
 /*
- * json-api (https://github.com/juliangut/json-api).
- * PSR-7 aware json-api integration.
+ * (c) 2018-2023 Julián Gutiérrez <juliangut@gmail.com>
  *
  * @license BSD-3-Clause
  * @link https://github.com/juliangut/json-api
- * @author Julián Gutiérrez <juliangut@gmail.com>
  */
 
 declare(strict_types=1);
@@ -15,21 +13,20 @@ use Jgut\ECS\Config\ConfigSet80;
 use PhpCsFixerCustomFixers\Fixer\NoNullableBooleanTypeFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-$header = <<<'HEADER'
-json-api (https://github.com/juliangut/json-api).
-PSR-7 aware json-api integration.
+return static function (ECSConfig $ecsConfig): void {
+    $header = <<<'HEADER'
+    (c) 2018-{{year}} Julián Gutiérrez <juliangut@gmail.com>
 
-@license BSD-3-Clause
-@link https://github.com/juliangut/json-api
-@author Julián Gutiérrez <juliangut@gmail.com>
-HEADER;
+    @license BSD-3-Clause
+    @link https://github.com/juliangut/json-api
+    HEADER;
 
-return static function (ECSConfig $ecsConfig) use ($header): void {
     $ecsConfig->paths([
         __FILE__,
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
+    $ecsConfig->cacheDirectory('.ecs.cache');
 
     (new ConfigSet80())
         ->setHeader($header)
--- src/Mapping/Attribute/Attribute.php
+++ src/Mapping/Attribute/Attribute.php
@@ -1,12 +1,10 @@
 <?php
 
 /*
- * json-api (https://github.com/juliangut/json-api).
- * PSR-7 aware json-api integration.
+ * (c) 2018-2023 Julián Gutiérrez <juliangut@gmail.com>
  *
  * @license BSD-3-Clause
  * @link https://github.com/juliangut/json-api
- * @author Julián Gutiérrez <juliangut@gmail.com>
  */
 
 declare(strict_types=1);
@@ -24,7 +22,7 @@ final class Attribute
         protected ?string $setter = null,
         /**
          * @var list<string>
-         */  protected array $groups = [],
+         */ protected array $groups = [],
     ) {}
 
     public function getName(): ?string
